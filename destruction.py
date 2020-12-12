7ff8f69b236a16f1b47588d905cf2430b43bfb06
Ilya <wickedbun@yandex.ru>
2020-12-12 13:21:33 +0500

    Yellow_Ellipce

---
+++ destruction.py
@@ -0,0 +1,41 @@
+import sys
+from random import randint
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.setWindowTitle('Git и желтые окружности')
+        self.flag = False
+        self.pushButton.clicked.connect(self.push)
+        self.col = randint(1, 7)
+
+    def push(self):
+        self.flag = True
+        self.repaint()
+
+    def paintEvent(self, event):
+        if self.flag:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw_flag(self, qp):
+        for i in range(randint(1, 15)):
+            x, y, r = randint(50, 250), randint(70, 350), randint(0, 50)
+            qp.setBrush(QColor('yellow'))
+            qp.drawEllipse(x, y, r, r)
+        self.flag = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
